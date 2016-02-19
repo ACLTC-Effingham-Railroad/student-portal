2068eb4ed99e7d58a0a2c5c1fcf6403a819c40fa
Cesar <cesarmejia451@gmail.com>
2016-02-18 20:18:30 -0600

    Mistakenly called our controller Students, but it was correctly changed to Pages. Setup routes and view pages for Pages controller. Cesar still rocks

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,15 @@
+class StudentsController < ApplicationController
+  
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :students
- root  'students#index'
+ devise_for :students
+ root 'pages#index'
+
+ get '/pages' => 'pages#index'
+ get '/pages/:id' => 'pages#show'
+
+ get '/pages/:id/edit' => 'pages#edit'
+ patch '/pages/:id' => 'pages#update'
+
 end
