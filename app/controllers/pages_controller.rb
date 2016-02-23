2cec1d696ac3f3dbf00404b210e116b7b6dacc08
Cesar <cesarmejia451@gmail.com>
2016-02-22 21:26:45 -0600

    Added form to Edit Page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -17,6 +17,7 @@ class PagesController < ApplicationController
   def update
     @pages = Unirest.patch("")headers:{ "Accept" => "application/json" }, parameters:{ first_name: params[:first_name], last_name: params[:last_name], email: params[:email], phone_number: params[:phone_number], short_bio: params[:short_bio], linkedin_url: params[:linkedin_url], twitter_handle: params[:twitter_handle], personal_blog_website_url: params[:personal_blog_website_url], online_resume_url: params[:online_resume_url], github_url: params[:github_url, photo: params[:photo]] }).body
 
+
     render :show
   end
   
